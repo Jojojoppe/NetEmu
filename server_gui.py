d586094d8897905d009c2446971d9895e644cd38
Joppe Blondel <joppe@blondel.nl>
2019-12-21 13:53:08 +0100

    Commented out gui

--- server_gui.py
+++ server_gui.py
@@ -15,17 +15,24 @@ class GuiThread(threading.Thread):
         # GUI data
         self.window_name = "NetEmu"
         self.gui_data = np.zeros([600,600,3])
+        self.zoom_level = 1.0
 
     def mouseCallback(self, event, x, y, flags, param):
-        self.gui_data[y,x,:] = [255,255,255]
+        pass
+        #self.gui_data[y,x,:] = [255,255,255]
 
     def run(self):
         # Render gui
         cv2.namedWindow(self.window_name)
         cv2.setMouseCallback(self.window_name, self.mouseCallback)
+
         while self.running:
+
+            # Write zoom level on left top
+            cv2.putText(self.gui_data, "zl: %f"%self.zoom_level, (10,10), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
             cv2.imshow(self.window_name, self.gui_data)
             cv2.waitKey(self.FPS_MS)
+
         cv2.destroyAllWindows()
 
     def stop(self):
